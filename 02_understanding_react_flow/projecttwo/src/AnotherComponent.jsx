7096f30729b907516892b74a19a607a245a08d88
hamza hanif <www.musamalik9@gmail.com>
2025-02-26 00:36:01 +0500

    understanding the react flow

---
+++ 02_understanding_react_flow/projecttwo/src/AnotherComponent.jsx
@@ -0,0 +1,9 @@
+function AnotherComponent(){
+    return(
+        <h2>
+            I am hamza and i love to code web and mobile applications !
+        </h2>
+    );
+}
+
+export default AnotherComponent;
---
+++ 02_understanding_react_flow/projecttwo/src/App.jsx
@@ -0,0 +1,15 @@
+import AnotherComponent from "./AnotherComponent"
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>Hello</h1>     
+    <AnotherComponent/>
+    </>
+   
+  )
+}
+
+export default App
